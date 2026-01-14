78da81ad57ec6bd87a494514f25c1793250c9811
public-release <as-public-release@janestreet.com>
2026-01-14 01:07:05 +0000

    v0.18~preview.130.83+317

--- src/lockfree_stack.mli
+++ src/lockfree_stack.mli
@@ -3,8 +3,11 @@ open! Base
 (** A lock-free stack of [once unique] elements. *)
 type !'a t
 
-(** [create ()] creates a new empty stack. *)
-val create : unit -> 'a t
+(** [create ()] creates a new empty stack.
+
+    The optional [padded] argument specifies whether to pad the data structure to avoid
+    false sharing. See {!Atomic.make} for a longer explanation. *)
+val create : ?padded:bool -> unit -> 'a t
 
 (** [is_empty t] determines whether the stack [t] is empty. *)
 val is_empty : 'a t -> bool
