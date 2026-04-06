e1c572204a8be0422b89654cd08ab3d997a6d3e8
public-release <as-public-release@janestreet.com>
2026-04-06 17:40:44 +0100

    v0.18~preview.130.91+190

--- src/once.mli
+++ src/once.mli
@@ -22,8 +22,10 @@ module Atomic : sig
   (** [make a] creates a reference to a [once unique] value [a]. *)
   val make : 'a @ contended once portable unique -> 'a t
 
-  (** [get_opt t] returns the value inside [t], or [None] if it was already accessed. *)
-  val get_opt : ('a : value). 'a t @ local -> 'a option @ contended once portable unique
+  (** [get_or_null t] returns the value inside [t], or [Null] if it was already accessed. *)
+  val get_or_null
+    : ('a : value).
+    'a t @ local -> 'a or_null @ contended once portable unique
 
   (** [get_exn t] returns the value inside [t], or raises [Failure] if it was already
       accessed. *)
